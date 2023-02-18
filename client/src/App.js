4636f4a2e8b739da6b9cbbf1c6a76657315b8ee5
MobiusXXF <104108778+MobiusXXF@users.noreply.github.com>
2023-02-18 19:09:53 +0000

    Added files for CloudResumeProject Playground. Experiment first with ideas before implementing, then transition to main repo for AWS Resume Project. Using the concept of branches to have a stable master with seperate branch/branches to implement features and bugfixes.

---
+++ client/src/App.js
@@ -0,0 +1,26 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <div>
+          <h1>Cloud Resume Challenge: Anthony</h1>
+          <h2>(<em>Under Construction</em>)</h2>
+        </div>
+        <a
+          className="App-link"
+          href="https://www.freecodecamp.org/MobiusXXF"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          FreeCodeCamp Profile
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
